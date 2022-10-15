adc02f8f97283af0bd90dc8a7e6b848359182810
XoneX <dominik.fh@web.de>
2022-10-15 18:28:44 +0200

    - add first gui layout with icon

--- gui.py
+++ gui.py
@@ -1,1 +1,15 @@
-dddd
+import sys
+
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication, QWidget
+
+app = QApplication(sys.argv)
+
+w = QWidget()
+w.setGeometry(50,50,700,400)
+w.setWindowTitle("Fuel-oil Web-Scraper")
+w.setWindowIcon(QIcon("heatingoil-icon.png"))
+
+w.show()
+
+sys.exit(app.exec())
